8068c17a450e81ce11763537c7f2e22837abd206
CAKGOD <17801030219@163.com>
2020-10-15 20:26:59 +0800

    add some codes

---
+++ code/Rust权威指南代码/第五章/fangfa/target/debug/deps/fangfa-3ee479f15bd97a02.d
@@ -0,0 +1,5 @@
+/home/cakgod/rust_learning/code/Rust权威指南代码/第五章/fangfa/target/debug/deps/fangfa-3ee479f15bd97a02: src/main.rs
+
+/home/cakgod/rust_learning/code/Rust权威指南代码/第五章/fangfa/target/debug/deps/fangfa-3ee479f15bd97a02.d: src/main.rs
+
+src/main.rs:
---
+++ code/Rust权威指南代码/第五章/fangfa/target/debug/fangfa.d
@@ -0,0 +1,1 @@
+/home/cakgod/rust_learning/code/Rust权威指南代码/第五章/fangfa/target/debug/fangfa: /home/cakgod/rust_learning/code/Rust权威指南代码/第五章/fangfa/src/main.rs
