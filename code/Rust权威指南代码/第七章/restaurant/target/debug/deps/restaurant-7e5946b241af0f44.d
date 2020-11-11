a4c03f3537875a6c495dc20e56232cde2e8a85d6
CAKGOD <17801030219@163.com>
2020-11-11 21:57:30 +0800

    add some codes in section 7

---
+++ code/Rust权威指南代码/第七章/restaurant/target/debug/deps/restaurant-7e5946b241af0f44.d
@@ -0,0 +1,7 @@
+/home/cakgod/rust_learning/code/Rust权威指南代码/第七章/restaurant/target/debug/deps/restaurant-7e5946b241af0f44.rmeta: src/lib.rs
+
+/home/cakgod/rust_learning/code/Rust权威指南代码/第七章/restaurant/target/debug/deps/librestaurant-7e5946b241af0f44.rlib: src/lib.rs
+
+/home/cakgod/rust_learning/code/Rust权威指南代码/第七章/restaurant/target/debug/deps/restaurant-7e5946b241af0f44.d: src/lib.rs
+
+src/lib.rs:
---
+++ code/Rust权威指南代码/第七章/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/cakgod/rust_learning/code/Rust权威指南代码/第七章/restaurant/target/debug/librestaurant.rlib: /home/cakgod/rust_learning/code/Rust权威指南代码/第七章/restaurant/src/lib.rs
